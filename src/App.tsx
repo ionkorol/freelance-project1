29d1ab19cdbb77bb5ecd922eed8b4c1e419ae3a3
Ion Sobol <ionsobol@gmail.com>
2021-09-22 16:56:11 -0400

    First

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Container, Form, Button } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Form>
+        <Form.Group>
+          <Form.Label>Where do you want to live?</Form.Label>
+          <Form.Select>
+            <option>Queens</option>
+            <option>Manhattan</option>
+            <option>Brooklyn</option>
+            <option>Staten Island</option>
+            <option>Bronx</option>
+          </Form.Select>
+        </Form.Group>
+        <img src="/brooklyn.jpg" width={500} height={500} alt="" />
+        <Form.Group>
+          <Form.Label>What is your current address?</Form.Label>
+          <Form.Control />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>What is your budget?</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>What amenities are you interested in?</Form.Label>
+          <Form.Select>
+            <option>Test Amenite</option>
+          </Form.Select>
+        </Form.Group>
+        <Button>Submit</Button>
+      </Form>
+    </Container>
   );
 }
 
